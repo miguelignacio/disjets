1869fa089edf97e09d2d581b33c9ee243c044f0d
bnachman <bnachman@cern.ch>
2021-08-24 19:58:22 +0000

    full results

---
+++ FinalReading/inputfiles/fullscan_stat/runmany2.py
@@ -0,0 +1,3 @@
+import os
+for i in range(200,220):
+    os.system("python unfold_fullstats_boot.py Rapgap nominal 0 "+str(i))
---
+++ FinalReading/inputfiles/fullscan_stat/runmany2.py~
@@ -0,0 +1,3 @@
+import os
+for i in range(1,20):
+    os.system("python unfold_fullstats_boot.py Rapgap nominal 0 "+str(i))
